d70ee8c0387cc59bbcfde6e0e88347f45dfe3c0e
einkurogane <59465679+einkurogane@users.noreply.github.com>
2024-04-11 02:27:16 -0300

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -10,3 +10,19 @@ document.addEventListener('DOMContentLoaded', (event) => {
     }
 });
 
+app.post('/upload', (req, res) => {
+    if (!req.files || Object.keys(req.files).length === 0) {
+        return res.status(400).send('No files were uploaded.');
+    }
+
+    // The name of the input field is used to retrieve the uploaded file
+    let uploadedVideo = req.files.videoUpload;
+
+    // Use the mv() method to place the file somewhere on your server
+    uploadedVideo.mv('/path/to/destination', function(err) {
+        if (err)
+            return res.status(500).send(err);
+
+        res.send('File uploaded!');
+    });
+});
